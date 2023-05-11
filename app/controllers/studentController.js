dc964ccf8004b6388493182534cd428b32c258e6
BenarroudjChristophe <christophe.benarroudj@oclock.school>
2023-05-11 19:32:57 +0200

    réussi

--- app/controllers/studentController.js
+++ app/controllers/studentController.js
@@ -3,29 +3,14 @@ const promoController = require("./promoController");
 
 const studentController = {
   studentDetail(req, res) {
-    const promo = students.filter((student) => student.promo == req.params.id);
-    for (const student of promo) {
-      let studentId = student.id;
-      let studentPromo = student.promo;
-      let studentGitHubUserName = student.github_username;
-      let studentFirstName = student.first_name;
-      let studentLastName = student.last_name;
-      let studentPicture = student.profile_picture_url;
+    const promoId = req.params.id;
 
-      // const studentId = req.params.id.students;
-      // console.log(studentId);
-      // const foundStudent = students.filter((student) => student.promo === promo);
+    const promo = students.filter((student) => student.promo == promoId);
 
-      // on envoie l'entrée trouvée à la vue
-      res.render("studentList", {
-        studentId,
-        studentPromo,
-        studentGitHubUserName,
-        studentFirstName,
-        studentLastName,
-        studentPicture,
-      });
-    }
+    // on envoie l'entrée trouvée à la vue
+    res.render("studentList", {
+      promo,
+    });
   },
 };
 
